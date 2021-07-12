5882d89ddd895b834bd20ed39c163e6030d5b5a8
SystematicError <systematicerror@users.noreply.github.com>
2021-07-12 20:32:31 +0530

    Replace hacky import function with importlib functions

--- src/main.py
+++ src/main.py
@@ -1,26 +1,24 @@
+from importlib.util import module_from_spec, spec_from_file_location
 from os.path import dirname, join
-from sys import path
 
 from blessed import Terminal
 
 
-def load_screen(screen: str, *args, **kwargs) -> None:
+def load_screen(screen_name: str, *args, **kwargs) -> None:
     """
     Loads a screen from the `screens` directory
 
     This function is merely a helper function to manage multiple screens.
-    It requires the screens directory be on path otherwise
-    python struggles to import it.
 
     Args:
-        screen (str): The name of the screen to load
+        screen_name (str): The name of the screen to load
     """
-    __import__(screen).on_screen_load(*args, **kwargs)
+    spec = spec_from_file_location('screens', join(dirname(__file__), f'screens/{screen_name}.py'))
+    screen = module_from_spec(spec)
+    spec.loader.exec_module(screen)
 
+    screen.on_screen_load(*args, **kwargs)
 
-# Adds the screens folder to path temporarily otherwise python can't
-# access your screens depending on the current directory
-path.append(join(dirname(__file__), "screens"))
 
 terminal = Terminal()
 menu_options = ["Play", "How to play", "Credits"]
