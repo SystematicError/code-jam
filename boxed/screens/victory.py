d35567bcfae3417fc9550e9aff7e929ca90fe876
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-07-17 18:53:39 +0200

    Center victory medal

--- boxed/screens/victory.py
+++ boxed/screens/victory.py
@@ -13,11 +13,13 @@ def display_game_over() -> None:
     print(boxed.terminal.yellow, end="")
     print(boxed.terminal.move_xy(boxed.terminal.width // 2 - 8, boxed.terminal.height // 2 - 8), end="")
 
-    for line in TROPHY.splitlines():
+    trophy_lines = TROPHY.splitlines()
+    for row, line in enumerate(trophy_lines):
         print(
-            boxed.terminal.move_down(1)
-            + boxed.terminal.move_left(19)
-            + line,
+            boxed.terminal.move_xy(
+                boxed.terminal.width // 2 - len(trophy_lines[0]) // 2,
+                row+boxed.terminal.height // 2 - len(trophy_lines) // 2,
+            ) + line,
             end="",
         )
     print(boxed.terminal.normal, end="")
